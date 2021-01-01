3646058c8110c3401b6a57f017b3e4133e85d318
takonomura <takonomura@users.noreply.github.com>
2021-01-01 18:29:55 +0900

    Implement basic functions

--- main.go
+++ main.go
@@ -1,10 +1,34 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"io"
 	"os"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
 )
 
 func main() {
-	fmt.Fprintf(os.Stderr, "Usage: %s\n", os.Args[0])
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s container_name\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
+
+	r, err := cli.ContainerAttach(context.Background(), os.Args[1], types.ContainerAttachOptions{Stdin: true, Stream: true})
+	if err != nil {
+		panic(err)
+	}
+	defer r.Close()
+
+	_, err = io.Copy(r.Conn, os.Stdin)
+	if err != nil {
+		panic(err)
+	}
 }
